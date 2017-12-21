4647eddde8d8c406c1d3b6148dcca0ff39fb7950
guillem <guillem.cordoba@gmail.com>
2017-12-21 14:12:24 +0100

    Formatted code

--- src/app/commons/services/location.service.ts
+++ src/app/commons/services/location.service.ts
@@ -3,23 +3,21 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class LocationService {
-
   getLocation(): Observable<any> {
     return Observable.create(observer => {
-        if(window.navigator && window.navigator.geolocation) {
-            window.navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    observer.next(position);
-                    observer.complete();
-                },
-                (error) => observer.error(error)
-            );
-        } else {
-            observer.error('Unsupported Browser');
-        }
+      if (window.navigator && window.navigator.geolocation) {
+        window.navigator.geolocation.getCurrentPosition(
+          position => {
+            observer.next(position);
+            observer.complete();
+          },
+          error => observer.error(error)
+        );
+      } else {
+        observer.error('Unsupported Browser');
+      }
     });
-}
+  }
 
-  constructor() { }
-
+  constructor() {}
 }
